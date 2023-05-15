13628628668e15715de1b2af5edaa3942c33fd4e
ruhul <ra.01403288711@gmail.com>
2023-05-15 16:57:16 +0600

    pg to mysql

---
+++ Database/TableCreate_mysql.sql
@@ -0,0 +1,32 @@
+CREATE TABLE pax_master (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    src INT,
+    airlines_code TEXT,
+    flight_date TEXT,
+    docs TEXT,
+    flight_code TEXT,
+    pax_id TEXT
+);
+
+CREATE TABLE pax_details (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    passport_no TEXT,
+    pax_id TEXT,
+    pax_name TEXT,
+    is_archived TEXT,
+    docs TEXT,
+    ticket_no TEXT
+);
+
+
+CREATE TABLE message_tbl (
+    e_subject TEXT,
+    e_sent_date TEXT,
+    e_from TEXT,
+    e_to TEXT,
+    pax_id TEXT,
+    msgbody TEXT,
+    status INT
+);
+
+
